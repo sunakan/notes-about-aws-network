9e7e78e01c24643f56fe68083180a4608c1b1789
sunakan <sunakan03@gmail.com>
2019-12-23 18:10:48 +0900

    chapter04
    - apache入れて起動
    - DNS ONしてドメイン取得

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "this" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
   tags = {
     Name = "VPC領域"
   }
